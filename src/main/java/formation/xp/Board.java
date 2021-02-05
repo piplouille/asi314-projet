3dfebbdf2ca7ffd34de976f67a0d6cd093031886
pierreeliseeflory <pierre-elisee.flory@ensta-paris.fr>
2021-02-05 16:05:16 +0100

    Refactored how the display of the Board is handled

--- src/main/java/formation/xp/Board.java
+++ src/main/java/formation/xp/Board.java
@@ -1,7 +1,8 @@
 package formation.xp;
 
 public class Board {
-    private Character[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+    private final Character[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+            'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
             'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
     protected int size;
     protected String name;
@@ -16,20 +17,8 @@ public class Board {
         }
     }
 
-    public Character[] getalphabet() {
-        return this.alphabet;
-    }
-
-    public void setalphabet(Character[] alphabet) {
-        this.alphabet = alphabet;
-    }
-
     public int getSize() {
         return this.size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
     }
 
     public String getName() {
@@ -40,34 +29,26 @@ public class Board {
         this.name = name;
     }
 
-    public Character[][] getBoats_array() {
-        return this.boats_array;
-    }
-
     public void setBoats_array(Character[][] boats_array) {
         this.boats_array = boats_array;
     }
 
-    private void printFirstLine() {
-        System.out.print("Navires :");
-        System.out.println("");
-        System.out.print("   ");
-        for (int i = 0; i < size; i++) {
-            System.out.print(alphabet[i].toString() + " ");
-        }
-        System.out.println("");
-    }
-
-    public void printBoard() {
-
-        this.printFirstLine();
+    public String toString() {
+        StringBuilder board = new StringBuilder();
+        board.append("Navires :\n   ");
+        for (int col = 0; col < size; col++)
+            board.append(alphabet[col].toString()).append(" ");
+        board.append("\n");
         for (int line = 1; line <= size; line++) {
-            System.out.print(line + "  ");
+            if (line >= 10)
+                board.append(line).append(" ");
+            else
+                board.append(line).append("  ");
             for (int col = 1; col <= size; col++) {
-                System.out.print(boats_array[line - 1][col - 1] + " ");
+                board.append(boats_array[line - 1][col - 1]).append(" ");
             }
-            System.out.println("");
+            board.append("\n");
         }
+        return board.toString();
     }
-
 }
